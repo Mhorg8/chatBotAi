96f7f29d17583266cad2b77de613634c27d50210
mohammad <mohammadalirezaie081@gmail.com>
2025-04-21 22:21:50 +0330

    create about page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
   return (
     <div className="w-full bg-zinc-900 py-4 px-4 border-b border-white/20 z-50">
       <div className="flex items-center justify-between w-full h-full">
-        <Link to="/" className="text-2xl text-white font-black">
+        <Link to="/" className="text-2xl text-white font-black border border-white rounded-md p-0.5">
           CHAT.AI
         </Link>
         <NavMenu />
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,61 @@
+import Header from "../components/Header";
+import { motion } from "framer-motion";
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  const animation = {
+    initial: {
+      opacity: 0,
+      scale: 0,
+    },
+    animate: {
+      opacity: 1,
+      scale: 1,
+    },
+  };
+
+  return (
+    <div className="font-roboto-bold overflow-hidden ">
+      <Header />
+      <div className="w-full h-[calc(100dvh-73px)] bg-zinc-900 flex flex-col items-start justify-center">
+        <motion.div
+          className="container mx-auto"
+          variants={animation}
+          initial="initial"
+          animate="animate"
+          transition={{ duration: 0.5  , type: "spring", bounce: 0.2 , stiffness: 100 }}
+        >
+          <h1 className="text-white font-extrabold text-7xl">ABOUT PROJECT</h1>
+          <p className="text-white text-lg">
+            This project was created for Azad University, but you can also use
+            it for daily use.{" "}
+          </p>
+          <p className="mt-10 mb-5 text-white text-4xl">Technologies Used</p>
+          <ul className="">
+            <li className="text-white flex items-center gap-2">
+              <h3 className="text-xl font-semibold">TypeScript : </h3>
+              <p>For the Front-end</p>
+            </li>
+            <li className="text-white flex items-center gap-2">
+              <h3 className="text-xl font-semibold">ReactJs : </h3>
+              <p>For the Front-end framework</p>
+            </li>
+            <li className="text-white flex items-center gap-2">
+              <h3 className="text-xl font-semibold">NodeJs :</h3>
+              <p>For the Back-end</p>
+            </li>
+            <li className="text-white flex items-center gap-2">
+              <h3 className="text-xl font-semibold">ExpressServer : </h3>
+              <p>For the local server</p>
+            </li>
+            <li className="text-white flex items-center gap-2">
+              <h3 className="text-xl font-semibold">PostgresSql : </h3>
+              <p>For the Database</p>
+            </li>
+          </ul>
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
